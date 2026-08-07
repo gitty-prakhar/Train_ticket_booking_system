34b430c26030a34bc8d4ceff79a0cfda7f64641b
Prakhar Gupta <prakharx.codes@gmail.com>
2026-08-07 10:26:54 +0530

    feat: add auth and error handler middlewares

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+import ApiError from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+export const verifyJWT=asyncHandler(async(req,_,res)=>{
+    try{
+        const token=req.cookies.accessToken||req.header("Authorization").replace("Bearer ","");
+        if(!token){
+            throw new ApiError(401,"Unauthorised Request\n");
+        }
+        const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
+        const user=await User.findById(decodedToken._id).select("-password -refreshToken");
+    
+        if(!user){
+            throw new ApiError(401,"Invalid Access Token\n");
+        }
+        req.user=user;
+        next();
+    }
+    catch(error){
+        throw new ApiError(401,error?.message||"Invalid Access Token\n");
+    }
+})
+
+export const verifyAdmin=(req,res,next)=>{
+    if(req.user&&req.user.role==="admin"){
+        next();
+    }
+    else{
+        throw new ApiError(403,"Access denied.Admins only.");
+    }
+}
