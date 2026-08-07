eac346ba1248fe48850f2b1f832a0fddaaa5c1e5
Prakhar Gupta <prakharx.codes@gmail.com>
2026-08-07 18:41:15 +0530

    fixbugs in seatLock

--- backend/src/utils/pnrGenerator.js
+++ backend/src/utils/pnrGenerator.js
@@ -1,18 +1,18 @@
 import { Booking } from "../models/booking.model.js";
 
 export const generateUniquePNR=async()=>{
-    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let pnr;
-    let isUnique = false;
+    let isUnique=false;
 
     while(!isUnique){   //jab tak isUnique false rhega tab tak
         const letter1=letters[Math.floor(Math.random()*26)];    //generates a random digit bw 0-25
         const letter2=letters[Math.floor(Math.random()*26)];    //generates a random digit bw 0-25
         const digits=Math.floor(1000000+Math.random()*9000000); //generate a random 7 digit letter
 
-        pnr = `${letter1}${letter2}${digits}`;  //9 digits unique pnr
+        pnr=`${letter1}${letter2}${digits}`;  //9 digits unique pnr
 
-        const existing = await Booking.findOne({pnr});
+        const existing=await Booking.findOne({pnr});
 
         if(!existing){
             isUnique=true;
